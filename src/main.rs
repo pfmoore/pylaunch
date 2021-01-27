0b42825c9405ddc21fb7fae6483b89041da8ca90
Paul Moore <p.f.moore@gmail.com>
2021-01-27 20:23:42 +0000

    Remove debugging statements

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,18 @@ fn main() {
     let exe = std::env::current_exe().unwrap();
     let stem = exe.file_stem().unwrap();
     let dir = exe.parent().unwrap();
+    let lib = dir.join("lib");
     let script = dir.join(stem).with_extension("py");
+    /*
     println!("Hello, world, from {:?} in {:?}!", stem, dir);
     println!("Running {:?}", script);
-    let status = Command::new("py").arg(script).status().expect("Failed");
-    println!("Status: {:?}", status);
+    println!("Running {:?}", std::env::args_os());
+    */
+    let status = Command::new("py")
+        .arg(script)
+        /* Skip the first arg, as it's the exe name! */
+        .args(std::env::args_os().skip(1))
+        .env("PYTHONPATH", lib)
+        .status().unwrap().code().unwrap_or(1);
+    std::process::exit(status);
 }
