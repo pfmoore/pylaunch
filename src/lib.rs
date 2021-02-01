c595f07bfb87af4f456d5c26f6146bebd63e3080
Paul Moore <p.f.moore@gmail.com>
2021-02-01 21:51:23 +0000

    Annotate code with types

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
 use std::process::Command;
 
 pub fn launch (ext: &str, cmd: &str) -> i32 {
+    // Result<PathBuf, std::io::Error>
     let exe = std::env::current_exe().unwrap();
+    // Option<&OsStr>
     let stem = exe.file_stem().unwrap();
+    // Option<&Path>
     let dir = exe.parent().unwrap();
     let lib = dir.join("lib");
     let script = dir.join(stem).with_extension(ext);
@@ -16,6 +19,8 @@ pub fn launch (ext: &str, cmd: &str) -> i32 {
         /* Skip the first arg, as it's the exe name! */
         .args(std::env::args_os().skip(1))
         .env("PYTHONPATH", lib)
-        .status().unwrap().code().unwrap_or(1);
+        .status() // std::io::Result<ExitStatus>
+        .unwrap().code() // Option<i32>
+        .unwrap_or(1);
     status
 }
