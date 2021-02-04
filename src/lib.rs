2d0f2a2f40be656daecbef0f6a85003d7d92b554
Paul Moore <p.f.moore@gmail.com>
2021-02-04 10:13:55 +0000

    More refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
 use std::process::Command;
 use std::path::{Path, PathBuf};
 
-fn find_python(exe: &Path, default: &str) -> PathBuf {
-    let dir = exe.parent().unwrap();
+fn find_python(dir: &Path, default: &str) -> PathBuf {
     let locs = &[".venv/Scripts", "embedded", "python"];
     for loc in locs {
+        // TODO: Should be pythonw for GUI version
         let interp = dir.join(loc).join("python.exe");
         // println!("{:?}", interp);
         if interp.exists() {
@@ -15,27 +15,23 @@ fn find_python(exe: &Path, default: &str) -> PathBuf {
 }
 
 fn find_script(exe: &Path, ext: &str) -> PathBuf {
-    // Option<&OsStr>
-    let stem = exe.file_stem().unwrap();
-    // Option<&Path>
-    let dir = exe.parent().unwrap();
-    let mut script = dir.join(stem);
+    let mut script = PathBuf::from(exe);
     script.set_extension(ext);
     script
 }
 
-fn find_lib(exe: &Path) -> PathBuf {
-    // Option<&Path>
-    let dir = exe.parent().unwrap();
+fn find_lib(dir: &Path) -> PathBuf {
     dir.join("lib")
 }
 
 pub fn launch (ext: &str, cmd: &str) -> i32 {
     // Result<PathBuf, std::io::Error>
-    let exe = std::env::current_exe().unwrap();
-    let interpreter = find_python(&exe, cmd);
+    let exe = std::env::current_exe().expect("Could not get current exe name");
+    // Option<&Path>
+    let dir = exe.parent().expect("Current exe does not have a directory");
+    let interpreter = find_python(&dir, cmd);
     let script = find_script(&exe, ext);
-    let lib = find_lib(&exe);
+    let lib = find_lib(&dir);
     /*
     println!("Hello, world, from {:?} in {:?}!", stem, dir);
     println!("Running {:?}", script);
