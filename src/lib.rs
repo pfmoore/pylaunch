431221b3f8ad4338d77f32abc0ebcd3fbea76498
Paul Moore <p.f.moore@gmail.com>
2021-02-21 11:09:40 +0000

    Explicitly define user configurable values

--- src/lib.rs
+++ src/lib.rs
@@ -7,26 +7,36 @@ use serde_json;
 
 
 #[derive(Deserialize)]
+struct UserConfig {
+    pub lib_location: Option<String>,
+    pub env_locs: Option<Vec<String>>,
+    pub script_locs: Option<Vec<String>>,
+}
+
+#[derive(Debug)]
 pub struct Config {
     pub exe_name: String,
     pub launcher_name: String,
+    pub extensions: Vec<String>,
     pub lib_location: String,
     pub env_locs: Vec<String>,
     pub script_locs: Vec<String>,
-    pub extensions: Vec<String>,
 }
 
 impl Config {
 
-    pub fn from_file<P: AsRef<Path>> (filename: P) -> Option<Config> {
+    pub fn from_file<P: AsRef<Path>> (filename: P, default: Config) -> Config {
+        let mut result = default;
         let file = filename.as_ref();
         if file.exists() {
             let contents = fs::read_to_string(filename)
                 .expect("Something went wrong reading the file");
-            Some(serde_json::from_str(&contents).unwrap())
-        } else {
-            None
+            let config: UserConfig = serde_json::from_str(&contents).unwrap();
+            if let Some(lib_location) = config.lib_location { result.lib_location = lib_location }
+            if let Some(env_locs) = config.env_locs { result.env_locs = env_locs }
+            if let Some(script_locs) = config.script_locs { result.script_locs = script_locs }
         }
+        result
     }
 
     fn find_python(&self, dir: &Path) -> PathBuf {
